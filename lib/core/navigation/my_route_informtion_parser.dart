91f6441e96cc595828e6c96e39ad71847ac710b7
Pawan Ghewande <ghewandepawan@gmail.com>
2021-06-03 19:20:08 +0530

    All set here, just need to add toggle button.

--- lib/core/navigation/my_route_informtion_parser.dart
+++ lib/core/navigation/my_route_informtion_parser.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 enum MyRoutes { first, second, third }
 
@@ -6,19 +7,16 @@ class MyRouteInformationParser extends RouteInformationParser<MyRoutes> {
   @override
   Future<MyRoutes> parseRouteInformation(
       RouteInformation routeInformation) async {
+    print('STATE OF APPS :: ${routeInformation.state}');
     switch (routeInformation.location) {
       case "/":
         return MyRoutes.first;
-        break;
       case "/second":
         return MyRoutes.second;
-        break;
       case "/third":
         return MyRoutes.third;
-        break;
       default:
         return MyRoutes.first;
-        break;
     }
   }
 
@@ -26,12 +24,22 @@ class MyRouteInformationParser extends RouteInformationParser<MyRoutes> {
   RouteInformation restoreRouteInformation(MyRoutes configuration) {
     switch (configuration) {
       case MyRoutes.first:
+        setLastRoute(configuration.index, '/');
         return const RouteInformation(location: '/');
       case MyRoutes.second:
+        setLastRoute(configuration.index, '/second');
         return const RouteInformation(location: '/second');
       case MyRoutes.third:
+        setLastRoute(configuration.index, '/third');
         return const RouteInformation(location: '/third');
     }
     // throw 'unknown';
+  }
+
+  setLastRoute(int visitedRoute, String location) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    print('Called Method to record last route.');
+    await prefs.setString('location', location);
+    await prefs.setInt('LastRoute', visitedRoute);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/navigation/my_route_delegate.dart';
 import 'core/navigation/my_route_informtion_parser.dart';
@@ -13,14 +14,38 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
-  MyRouterDelegate _delegate = MyRouterDelegate();
+  /// This widget is the root of your application.
+  final MyRouterDelegate _delegate = MyRouterDelegate();
+  final MyRouteInformationParser _myRouteParser = MyRouteInformationParser();
+  var lastLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    // checkLastRoute();
+    _delegate.setInitialRoutePath(MyRoutes.values[1]);
+    // print('data {$lastLocation}');
+    // _myRouteParser.parseRouteInformation(RouteInformation(location: ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Flutter Demo',
-      routeInformationParser: MyRouteInformationParser(),
+      title: 'Navigator 2.0',
+      routeInformationParser: _myRouteParser,
       routerDelegate: _delegate,
     );
+  }
+
+  checkLastRoute() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int index = (prefs.getInt('LastRoute') ?? 0);
+    print(index);
+    // return index;
+    // (Router.of(context).routerDelegate as MyRouterDelegate).configuration =
+    //     index as MyRoutes;
+    String location = (prefs.getString('location') ?? '/');
+    print(location);
+    //return location;
   }
 }
